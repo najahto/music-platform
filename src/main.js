036271b9bf3bf2a4588716a076be58ef696305ee
Taha Omar Najah <tahaomarnajah@gmail.com>
2021-10-05 13:56:58 +0100

    added template

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+/* eslint-disable eol-last */
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './assets/tailwind.css';
+import './assets/main.css';
 
 createApp(App).use(store).use(router).mount('#app');
